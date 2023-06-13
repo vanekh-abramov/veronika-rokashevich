01aaea9a00270174e49005524081c2e40482ec86
Vania <vanekh.abramov@gmail.com>
2023-06-14 00:08:42 +0300

    feat: add image slider

--- src/pages/subGallery/SubGallery.jsx
+++ src/pages/subGallery/SubGallery.jsx
@@ -1,11 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import classes from "./SubGallery.module.scss";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { host } from "../../assets/links/host";
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./styles.css";
+
+// import required modules
+import { Pagination, Navigation } from "swiper";
+
 const SubGallery = () => {
   const [data, setData] = useState();
+  const [open, setOpen] = useState(false);
 
   const params = useParams();
 
@@ -24,21 +38,69 @@ const SubGallery = () => {
     (image) => image?.attributes
   );
 
+  const openToggle = () => {
+    setOpen(true);
+  };
+
+  const closeToggle = () => {
+    setOpen(false);
+  };
+
   return (
-    <div className={classes.subgallery_wrapper}>
-      {additional?.map((el) => {
-        return (
-          <div className={classes.image_box}>
-            <img src={host + el?.image?.data?.[0]?.attributes?.url} alt='' />
-            <div className={classes.sub_image}>
-              <p className={classes.title}>{el.title}</p>
-              <p className={classes.param}>{el.size}</p>
-              <p className={classes.param}>{el.texture}</p>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      {open ? (
+        <div className={classes.absolute_wrapper}>
+          <Swiper
+            pagination={{
+              type: "fraction",
+            }}
+            navigation={true}
+            modules={[Pagination, Navigation]}
+            className='mySwiper'
+          >
+            {additional?.map((el) => {
+              return (
+                <div className={classes.image_box}>
+                  <SwiperSlide key={el?.image?.data?.[0]?.attributes?.url}>
+                    <img
+                      className={classes.slider_image}
+                      onClick={openToggle}
+                      src={host + el?.image?.data?.[0]?.attributes?.url}
+                      alt=''
+                    />
+                    <div className={classes.sub_image}>
+                      <p className={classes.title}>{el.title}</p>
+                      <p className={classes.param}>{el.size}</p>
+                      <p className={classes.param}>{el.texture}</p>
+                    </div>
+                  </SwiperSlide>
+                </div>
+              );
+            })}
+            <div onClick={closeToggle} className={classes.close_swiper}></div>
+          </Swiper>
+        </div>
+      ) : (
+        <div className={classes.subgallery_wrapper}>
+          {additional?.map((el) => {
+            return (
+              <div className={classes.image_box}>
+                <img
+                  onClick={openToggle}
+                  src={host + el?.image?.data?.[0]?.attributes?.url}
+                  alt=''
+                />
+                <div className={classes.sub_image}>
+                  <p className={classes.title}>{el.title}</p>
+                  <p className={classes.param}>{el.size}</p>
+                  <p className={classes.param}>{el.texture}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 };
 
